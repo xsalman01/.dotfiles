a20828402404623d607bfa801334e7fb4032d115
xsalman01 <sasalman00@gmail.com>
2025-03-11 22:54:18 +0500

    bar font adjustment

--- xborders/launch_borders.sh
+++ xborders/launch_borders.sh
@@ -3,8 +3,8 @@
 pkill xborders
 
 xborders --border-radius 2 \
-    --border-width 1 \
-    --border-mode center \
-    --border-rgba \#ebbcba \
+    --border-width 2 \
+    --border-mode outside \
+    --border-rgba \#4C4870ff \
     --disable-version-warning &
 exit 0

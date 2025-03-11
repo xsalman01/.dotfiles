aeebdde8bac51b96ea6c9f902822be07f840d68f
xsalman01 <sasalman00@gmail.com>
2025-03-11 21:49:21 +0500

    rosepined

--- xborders/launch_borders.sh
+++ xborders/launch_borders.sh
@@ -3,8 +3,8 @@
 pkill xborders
 
 xborders --border-radius 2 \
-    --border-width 2 \
+    --border-width 1 \
     --border-mode center \
-    --border-rgba \#c4a7e7 \
+    --border-rgba \#ebbcba \
     --disable-version-warning &
 exit 0

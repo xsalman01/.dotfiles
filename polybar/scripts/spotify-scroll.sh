ef55e9db628661be31126a3c8b4b9ab35297e30e
xsalman01 <sasalman00@gmail.com>
2025-04-06 20:00:52 +0500

    all ok

--- polybar/scripts/spotify-scroll.sh
+++ polybar/scripts/spotify-scroll.sh
@@ -4,7 +4,7 @@ zscroll -l 30 \
         --delay 0.15 \
         --scroll-padding " | " \
         --match-command "`dirname $0`/spotify-status.sh --status" \
-        --match-text "Playing" "--scroll 1 --after-text ' %{T6}%{T-}'" \
-        --match-text "Paused" "--scroll 0 --after-text ' %{T6}%{T-}'" \
+        --match-text "Playing" "--scroll 1 --after-text ' %{T6 F#f6c177}%{T- F-}'" \
+        --match-text "Paused" "--scroll 0 --after-text ' %{T6 F#f6c177}%{T- F-}'" \
         --update-check true "`dirname $0`/spotify-status.sh" &
 wait

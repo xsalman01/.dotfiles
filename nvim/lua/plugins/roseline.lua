569602648513df568f44df6dd34c903cae0adc67
Agnosia <sasalman00@gmail.com>
2025-05-07 13:56:47 +0500

    fixed keyboard issue and added directory path to nvim status line

--- nvim/lua/plugins/roseline.lua
+++ nvim/lua/plugins/roseline.lua
@@ -1,6 +1,40 @@
+local function get_file_path()
+  local current_file = vim.fn.expand('%:p')
+  
+  if current_file == "" then
+    return ""
+  end
+  
+  local git_dir = vim.fn.finddir('.git', vim.fn.expand('%:p:h') .. ';')
+  
+  local relative_path = ""
+  
+  if git_dir ~= "" then
+    local project_root = vim.fn.fnamemodify(git_dir, ':h')
+    relative_path = vim.fn.fnamemodify(current_file, ':s?' .. project_root .. '/??')
+  else
+    local home_dir = vim.fn.expand('~')
+    if vim.fn.stridx(current_file, home_dir) == 0 then
+      relative_path = '~' .. current_file:sub(#home_dir + 1)
+    else
+      relative_path = current_file
+    end
+  end
+  
+  return "%#Directory#%* " .. relative_path
+end
+
 return {
     'maxmx03/roseline',
-    opts = {},
+    opts = {
+        layout = {
+            a = section_a,
+            b = section_b,
+            c = get_file_path,
+            d = section_d,
+            e = section_e,
+        }
+    },
     dependencies = {
       'rose-pine/neovim',
     }

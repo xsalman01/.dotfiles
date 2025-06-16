7f8dc89bce81086a89244b3c6265c6af3c0040a7
xsalman01 <sasalman00@gmail.com>
2025-06-16 22:29:57 +0500

    terminal prompt colored

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,17 +3,21 @@
 # Enable prompt substitution
 setopt PROMPT_SUBST
 
-
-
 ZVM_CURSOR_STYLE_ENABLED=false
 
 # Git branch info
 autoload -Uz vcs_info
-precmd() { vcs_info }
+precmd() { 
+    vcs_info
+    print ""
+}
 zstyle ':vcs_info:git:*' formats 'on %b'
 
 # Left prompt: breadcrumb pat
-PROMPT='${BG_FOAM} ${FG_TEXT}${VI_PROMPT_MODE}'\
-' ${FOLDER_ICON} %~ ${RESET}${FG_PINE}${SEGMENT_SEPARATOR}${RESET}'\
-'${vcs_info_msg_0_:+${FG_ROSE}${BRANCH_ICON} ${vcs_info_msg_0_}${RESET}}
- ${FG_GOLD}${PROMPT_SYMBOL} '
+PROMPT='${VI_PROMPT_MODE}${FG_LOVE}${BACKWARD_SEPARATOR}${BG_LOVE}${FG_SURFACE}'\
+' %B${FOLDER_ICON}${${PWD/#$HOME//}//\//${PATH_SEPARATOR}} %b'\
+'${RESET}${FG_LOVE}${FORWARD_SEPARATOR}${RESET}'\
+'${vcs_info_msg_0_:+${FG_PINE}${BACKWARD_SEPARATOR}${BG_PINE}${FG_GOLD} '\
+'${BRANCH_ICON} ${vcs_info_msg_0_} ${RESET}${FG_PINE}${FORWARD_SEPARATOR}${RESET}}
+
+${FG_GOLD}${PROMPT_SYMBOL} '

ef6e83ab48f2b0381a2c93099dcc5b1ee2c4b082
Agnosia <sasalman00@gmail.com>
2025-07-01 12:37:35 +0500

    vim hover borders

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -33,3 +33,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "K", function()
+    vim.lsp.buf.hover({ border = "rounded" })
+end)

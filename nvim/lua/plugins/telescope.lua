36d8e4059649b851e42ee8b6c12131a3dd4ee9ff
xsalman01 <sasalman00@gmail.com>
2025-07-16 19:18:44 +0500

    telescope copy text

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,14 +8,44 @@ return {
     },
     cmd = { "Telescope", "TelescopeFindFiles", "TelescopeBuffers", "TelescopeLiveGrep" },
     config = function()
-        require('telescope').setup({})
+        local action_state = require("telescope.actions.state")
+
+        local yank_display = function(prompt_bufnr)
+            local picker = action_state.get_current_picker(prompt_bufnr)
+            local row = picker:get_selection_row()
+            local entry = picker.manager:get_entry(picker:get_index(row))
+            local display = entry.display
+            if type(display) == 'function' then
+                local _display_highlight
+                display, _display_highlight = entry:display()
+            end
+
+            if type(display) == 'string' then
+                vim.fn.setreg('+', display)
+            else
+                print("Could not extract display text.\n")
+            end
+        end
+
+        require('telescope').setup({
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<C-y>"] = yank_display,
+                    },
+                    n = {
+                        ["<C-y>"] = yank_display,
+                    },
+                },
+            },
+        })
 
         local builtin = require('telescope.builtin')
 
         vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") });
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
     end
 }

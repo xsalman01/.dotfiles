6b15de069dcc485527be6af40a92fae989d6cfcd
Salman <sasalman00@gmail.com>
2025-09-29 14:03:44 +0500

    removed lazydocker nvim

--- nvim/lua/plugins/toggleterm.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'akinsho/toggleterm.nvim',
-  event = 'ColorScheme',
-  config = function()
-    local highlights = require('rose-pine.plugins.toggleterm')
-    require('toggleterm').setup({ highlights = highlights })
-  end
-}

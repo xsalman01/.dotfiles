9906763386e6273fbd4a4b8f0f8b63fde5c90d85
Salman <sasalman00@gmail.com>
2025-10-22 19:14:12 +0500

    waybar config

--- zsh/envs.zsh
+++ zsh/envs.zsh
@@ -5,3 +5,4 @@ export VISUAL=/usr/bin/nvim
 export _ZO_RESOLVE_SYMLINKS=0
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
+export XDG_CONFIG_HOME=$HOME/.config

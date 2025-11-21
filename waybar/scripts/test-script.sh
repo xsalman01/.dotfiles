0eb35bd0f9790029272737163a39d8dbe81ce79c
Salman <sasalman00@gmail.com>
2025-11-21 17:11:22 +0500

    some waybar testing configs

---
+++ waybar/scripts/test-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Waybar test script executed at $(date '+%H:%M:%S')" >> /tmp/waybar-signal-log
+echo "Signal test ran at $(date '+%H:%M:%S')"
+echo "-Signal test ran at $(date '+%H:%M:%S')"
+echo "--Signal test ran at $(date '+%H:%M:%S')"
+echo "---Signal test ran at $(date '+%H:%M:%S')"
+echo "----Signal test ran at $(date '+%H:%M:%S')"
+

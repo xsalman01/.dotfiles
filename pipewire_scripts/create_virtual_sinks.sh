2bb4eddd55204bd7ba2068373536cab0cfb4340b
xsalman01 <sasalman00@gmail.com>
2026-01-18 02:54:24 +0500

    noctalia: added and configured a bit

--- pipewire_scripts/create_virtual_sinks.sh
+++ pipewire_scripts/create_virtual_sinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 2
+sleep 1
 
 REAL_SINK="$(pactl get-default-sink)"
 
@@ -16,6 +16,24 @@ pactl list short sinks | grep -q "chat_sink" || \
     sink_name=chat_sink \
     sink_properties=device.description=Chat
 
+# ---------- set default sink only if needed ----------
+
+CURRENT_DEFAULT="$(pactl get-default-sink)"
+
+if [ "$CURRENT_DEFAULT" != "game_sink" ]; then
+  pactl set-default-sink game_sink
+
+  # # give PA/PW time to auto-move streams
+  # sleep 0.2
+
+  # # ---------- move loopbacks back to real sink ----------
+  # pactl list short sink-inputs | awk '
+  #   /game_sink\.monitor|chat_sink\.monitor/ {print $1}
+  # ' | while read -r id; do
+  #   pactl move-sink-input "$id" "$REAL_SINK"
+  # done
+fi
+
 # ---------- create loopbacks (once) ----------
 
 pactl list short modules | grep -q "module-loopback.*game_sink.monitor" || \
@@ -27,22 +45,4 @@ pactl list short modules | grep -q "module-loopback.*chat_sink.monitor" || \
   pactl load-module module-loopback \
     source=chat_sink.monitor \
     sink="$REAL_SINK"
-
-# ---------- set default sink only if needed ----------
-
-CURRENT_DEFAULT="$(pactl get-default-sink)"
-
-if [ "$CURRENT_DEFAULT" != "game_sink" ]; then
-  pactl set-default-sink game_sink
-
-  # give PA/PW time to auto-move streams
-  sleep 0.2
-
-  # ---------- move loopbacks back to real sink ----------
-  pactl list short sink-inputs | awk '
-    /game_sink\.monitor|chat_sink\.monitor/ {print $1}
-  ' | while read -r id; do
-    pactl move-sink-input "$id" "$REAL_SINK"
-  done
-fi
 

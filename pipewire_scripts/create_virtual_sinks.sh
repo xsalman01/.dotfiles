5bfb68001e70406379a456c3e5f7f3c260731224
xsalman01 <sasalman00@gmail.com>
2026-01-18 21:15:20 +0500

    noctalia: some settings

--- pipewire_scripts/create_virtual_sinks.sh
+++ pipewire_scripts/create_virtual_sinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 1
+sleep 3
 
 REAL_SINK="$(pactl get-default-sink)"
 
@@ -22,16 +22,6 @@ CURRENT_DEFAULT="$(pactl get-default-sink)"
 
 if [ "$CURRENT_DEFAULT" != "game_sink" ]; then
   pactl set-default-sink game_sink
-
-  # # give PA/PW time to auto-move streams
-  # sleep 0.2
-
-  # # ---------- move loopbacks back to real sink ----------
-  # pactl list short sink-inputs | awk '
-  #   /game_sink\.monitor|chat_sink\.monitor/ {print $1}
-  # ' | while read -r id; do
-  #   pactl move-sink-input "$id" "$REAL_SINK"
-  # done
 fi
 
 # ---------- create loopbacks (once) ----------
@@ -39,7 +29,7 @@ fi
 pactl list short modules | grep -q "module-loopback.*game_sink.monitor" || \
   pactl load-module module-loopback \
     source=game_sink.monitor \
-    sink="$REAL_SINK"
+    sink=$REAL_SINK
 
 pactl list short modules | grep -q "module-loopback.*chat_sink.monitor" || \
   pactl load-module module-loopback \

e1b6752e5a0b21da7473376767915cf6083f4fc6
xsalman01 <sasalman00@gmail.com>
2026-03-26 21:44:42 +0500

    idk some shit

--- hypr/float_extensions.sh
+++ hypr/float_extensions.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-while read -r event_data; do
-  event="${event_data%%>>*}"
-  edata="${event_data#"$event">>}"
 
-  if [[ "$event" == "windowtitlev2" ]]; then
-    IFS=',' read -r -a fields <<<"$edata"
-    if [[ "${fields[1]}" =~ ^Extension:\ \( ]]; then
-     hyprctl dispatch setfloating address:"0x${fields[0]}" && hyprctl dispatch resizewindowpixel "exact 350 580",address:"0x${fields[0]}" && hyprctl dispatch movewindowpixel "14 20",address:"0x${fields[0]}"
-    fi
-  fi
-done
+if [ "$(hyprctl activeworkspace -j | jq -r '.tiledLayout')" = "master" ]; then
+    hyprctl dispatch layoutmsg "setlayout scroller"
+else
+    hyprctl dispatch layoutmsg "setlayout master"
+fi

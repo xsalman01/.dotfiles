bfad100a554f1ce2058d2424ae9166bfb5f4e7fc
Salman <sasalman00@gmail.com>
2026-04-07 12:20:52 +0500

    nvim: treesitter migration

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,32 +1,31 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
+    branch = "main",
 
-    config = function ()
+    config = function()
+        require('nvim-treesitter').setup {}
 
-        require ('nvim-treesitter.configs').setup {
-            -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-            ensure_installed = { "javascript", "typescript", "go",
-                "html", "css", "lua", "vim", "vimdoc",
-                "bash", "astro", "markdown", "markdown_inline", "qmljs"
-            },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            highlight = {
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
+        local ensureInstalled = { "javascript", "typescript",
+            "html", "css", "lua", "vim", "vimdoc",
+            "bash", "markdown", "markdown_inline"
         }
-    end
+        local ts = require('nvim-treesitter')
+        local parsersToInstall = vim.iter(ensureInstalled)
+            :filter(function(p)
+                return not vim.tbl_contains(ts.get_installed(), p)
+            end)
+            :totable()
+        if #parsersToInstall > 0 then
+            ts.install(parsersToInstall)
+        end
+
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                pcall(vim.treesitter.start)
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
+    end,
 }
